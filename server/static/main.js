55c8623fcfb9545f7ecc3260f302d1cd5c2bd22a
Robosword <32asingh@stratfordschools.net>
2026-05-18 16:56:57 -0700

    Create the main.js

--- server/static/main.js
+++ server/static/main.js
@@ -1,1 +1,26 @@
+async function sendData(){
+      var sendData = {
+        "1": document.getElementById("1").value,
+        "2": document.getElementById("2").value,
+        "3": document.getElementById("3").value,
+        "4": document.getElementById("4").value,
+        "5": document.getElementBYId("5").value
+      }
+       fetch('/sendHandAngles', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(sendData) 
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+ 
+    }
 
+setInterval(sendData,100);
