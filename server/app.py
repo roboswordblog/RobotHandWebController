1c7a857c31f3c0a25f38a3367bf55f96f1441c5b
Robosword <32asingh@stratfordschools.net>
2026-05-18 16:22:22 -0700

    Get the data from the server

--- server/app.py
+++ server/app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
+@app.route("/sendHandAngles")
+def getData():
+    one = request.get_json("1")
+    two = request.get_json("2")
+    three = request.get_json("3")
+    four = request.get_json("4")
+    five = request.get_json("5")
+    
 if __name__ == "__main__":
   app.run(debug=True)
